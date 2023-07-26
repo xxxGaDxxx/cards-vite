08cc7331879f1630c4ed13e34ea7056d46935b37
Wlad loban <qwegadqwe@gmail.com>
2023-07-26 09:13:34 +0300

    add styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App.tsx'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
