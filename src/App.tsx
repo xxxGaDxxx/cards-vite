1702cc771d0d58dd9bb5637c5f7fd8932f8c7de8
Wlad loban <qwegadqwe@gmail.com>
2023-07-27 10:19:55 +0300

    test new branch yml

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,9 @@ export function App() {
         <Typography as={'span'} variant={'body2'}>
           asdwasd
         </Typography>
+        <Typography as={'span'} variant={'body2'}>
+          asdwasd
+        </Typography>
       </Card>
     </div>
   )
