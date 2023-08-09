41b1ec87444ce0f48a3b6c5b4d38a120249b93ad
Wlad loban <qwegadqwe@gmail.com>
2023-08-09 11:55:53 +0300

    refactoring dropDown

--- src/components/ui/dropDown/dropDown.stories.tsx
+++ src/components/ui/dropDown/dropDown.stories.tsx
@@ -3,37 +3,37 @@ import { Meta, StoryObj } from '@storybook/react'
 import { Profile, SignOut } from '../../../assets/icons'
 import { Avatar } from '../avatar'
 
-import { DropDown } from './dropDown.tsx'
+import { Dropdown, DropdownItemWithIcon } from '.'
 
 const meta = {
-  title: 'Components/Drop Down',
-  component: DropDown,
+  title: 'Components/Dropdown',
   tags: ['autodocs'],
-} satisfies Meta<typeof DropDown>
+} satisfies Meta<typeof Dropdown>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const AvaProfile: Story = {
-  args: {
-    label: 'Name',
-    dataMenu: [
-      {
-        id: '2',
-        label: 'My Profile',
-        icon: <Profile />,
-      },
-      {
-        id: '3',
-        label: 'Sign Out',
-        icon: <SignOut />,
-      },
-    ],
-    icon: (
-      <Avatar
-        src={'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80'}
-        name={'Vlad'}
-      />
-    ),
+export const WithTriggerButton: Story = {
+  render: args => {
+    return (
+      <div>
+        <Dropdown
+          trigger={
+            <button>
+              <Avatar
+                src={'https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg'}
+                name={'Vlad'}
+              />
+            </button>
+          }
+          {...args}
+        >
+          <>
+            <DropdownItemWithIcon icon={<Profile />} text="Learn" onSelect={() => {}} />
+            <DropdownItemWithIcon icon={<SignOut />} text="Delete" onSelect={() => {}} />
+          </>
+        </Dropdown>
+      </div>
+    )
   },
 }
