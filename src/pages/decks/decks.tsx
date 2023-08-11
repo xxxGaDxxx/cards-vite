5b4af0fa71568ab61b5f2eae0ca6a448bb263524
Wlad loban <qwegadqwe@gmail.com>
2023-08-11 14:37:33 +0300

    refactoring

--- src/pages/decks/decks.tsx
+++ src/pages/decks/decks.tsx
@@ -1,16 +1,7 @@
 import { useEffect } from 'react'
 
-import { Edit, PlayCircle, Trash } from '../../assets/icons'
 import { useDebounce } from '../../common/hooks/useDebounce.tsx'
-import {
-  Button,
-  ColumnType,
-  Page,
-  SortType,
-  Table,
-  TabsItemsType,
-  Pagination,
-} from '../../components'
+import { Page, Pagination, SortType, TabsItemsType } from '../../components'
 import { useGetDecksQuery } from '../../services/decks/decks.ts'
 import {
   decksSliceActions,
@@ -24,7 +15,7 @@ import {
 import { useAppDispatch, useAppSelector } from '../../store/store.ts'
 import { SortingComponent } from '../components/sortingComponent'
 
-import s from './decks.module.scss'
+import { TableDecks } from './tableDecks'
 
 const DEFAULT_CARDS_COUNT = 10
 
@@ -56,7 +47,9 @@ export const Decks = () => {
 
   const orderByRow = orderBy ? `${orderBy.key}-${orderBy.direction}` : ''
 
-  const { data: decks } = useGetDecksQuery({
+  const isAllPack = tabActive.key === 'all'
+
+  const { data: decks, isSuccess } = useGetDecksQuery({
     minCardsCount: sliderValue[0],
     maxCardsCount: sliderValue[1],
     name: debounceText,
@@ -78,100 +71,47 @@ export const Decks = () => {
   }, [decks?.maxCardsCount])
 
   return (
-    <Page>
-      <SortingComponent
-        titlePage={' Packs List'}
-        maxCardsCount={decks?.maxCardsCount || DEFAULT_CARDS_COUNT}
-        optionsTabs={optionsTabs}
-        tabActive={tabActive}
-        searchByName={searchByName}
-        sliderValue={sliderValue}
-        onAddNew={() => {}}
-        onClearFilter={onClearFilter}
-        onSearch={setSearchByName}
-        onSliderChange={setSliderValue}
-        onTabsItem={setTabActive}
-      />
-      <div className={s.containerTable}>
-        <Table.Root style={{ width: '100%' }}>
-          <Table.Header columns={columns} onSort={setOrderBy} sort={orderBy} />
-          <Table.Body>
-            {decks?.items &&
-              decks?.items.map(deck => (
-                <Table.Row key={deck?.id}>
-                  <Table.Cell>
-                    <Button variant={'link'} className={s.button}>
-                      {deck?.name}
-                    </Button>
-                  </Table.Cell>
-                  <Table.Cell>{deck.cardsCount}</Table.Cell>
-                  <Table.Cell>{new Date(deck.updated).toLocaleDateString('en-GB')}</Table.Cell>
-                  <Table.Cell>{deck?.author.name}</Table.Cell>
-                  {/*TODO открытие модалки или переход на изучение*/}
-                  {tabActive.key === 'all' ? (
-                    <Table.Cell className={s.containerIcon}>
-                      <Button variant={'link'}>
-                        <PlayCircle />
-                      </Button>
-                    </Table.Cell>
-                  ) : (
-                    <Table.Cell className={s.containerIcon}>
-                      <Button variant={'link'} className={s.buttonIcon}>
-                        <PlayCircle />
-                      </Button>
-                      <Button variant={'link'} className={s.buttonIcon}>
-                        <Edit />
-                      </Button>
-                      <Button variant={'link'}>
-                        <Trash />
-                      </Button>
-                    </Table.Cell>
-                  )}
-                </Table.Row>
-              ))}
-          </Table.Body>
-        </Table.Root>
-      </div>
-      <Pagination
-        count={decks?.pagination.totalPages || itemsPerPage}
-        page={decks?.pagination?.currentPage || currentPage}
-        onChange={setCurrentPage}
-        onPerPageChange={setItemsPerPage}
-        perPage={decks?.pagination.itemsPerPage}
-        perPageOptions={[4, 7, 10]}
-      />
-    </Page>
+    <>
+      <Page>
+        {isSuccess && (
+          <>
+            <SortingComponent
+              titlePage={' Packs List'}
+              maxCardsCount={decks?.maxCardsCount || DEFAULT_CARDS_COUNT}
+              optionsTabs={optionsTabs}
+              tabActive={tabActive}
+              searchByName={searchByName}
+              sliderValue={sliderValue}
+              onAddNew={() => {}}
+              onClearFilter={onClearFilter}
+              onSearch={setSearchByName}
+              onSliderChange={setSliderValue}
+              onTabsItem={setTabActive}
+            />
+
+            <TableDecks
+              orderBy={orderBy}
+              decksItems={decks?.items}
+              setOrderBy={setOrderBy}
+              isAllPack={isAllPack}
+            />
+
+            <Pagination
+              count={decks?.pagination.totalPages || itemsPerPage}
+              page={decks?.pagination?.currentPage || currentPage}
+              onChange={setCurrentPage}
+              onPerPageChange={setItemsPerPage}
+              perPage={decks?.pagination.itemsPerPage}
+              perPageOptions={[4, 7, 10]}
+            />
+          </>
+        )}
+      </Page>
+    </>
   )
 }
 
 const optionsTabs: TabsItemsType[] = [
   { key: 'my', label: 'My Cards' },
   { key: 'all', label: 'All Cards' },
-]
-
-const columns: ColumnType[] = [
-  {
-    key: 'name',
-    title: 'Name',
-    sortable: true,
-  },
-  {
-    key: 'cardsCount',
-    title: 'Cards',
-    sortable: true,
-  },
-  {
-    key: 'updated',
-    title: 'Last Updated',
-    sortable: true,
-  },
-  {
-    key: 'created',
-    title: 'Created by',
-    sortable: true,
-  },
-  {
-    key: 'options',
-    title: '',
-  },
 ]
